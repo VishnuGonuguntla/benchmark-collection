7861ed348ec0e454a074794332a5b6949eb2c659
VishnuGonuguntla <grajavishnu@gmail.com>
2026-09-24 03:49:25 +0200

    Modified config.mk

--- config.mk
+++ config.mk
@@ -1,8 +1,12 @@
-INTEL_MKL_PATH = "/apps/SPACK/0.23.1/opt/linux-ubuntu24.04-x86_64_v3/gcc-13.3.0/intel-oneapi-mkl-2024.2.2-ypxe56o6gvnp36cijjhjl2ajfusjriqt/mkl/2024.2/"
+TOOLCHAIN ?= ICX # ICX/GCC/AOCC/NVCC
+OPTIMIZATION ?= NAIVE # NAIVE/VENDOR/BLAS
+PRECISION ?= SP # SP/DP
+
+BUILD_VERBOSE ?= 1
+RUN_VERBOSE ?= 1
+PRECISION ?= DP
 
 #path for mpiicx or mpicc
 CC = "/apps/SPACK/0.23.1/opt/linux-ubuntu24.04-x86_64_v3/gcc-13.3.0/openmpi-5.0.5-dgprja75do5kyxfsurjxcb5dvbdcks3c/bin/mpicc"
 #path for mpiicpx or mpicxx
 CXX = "/apps/SPACK/0.23.1/opt/linux-ubuntu24.04-x86_64_v3/gcc-13.3.0/openmpi-5.0.5-dgprja75do5kyxfsurjxcb5dvbdcks3c/bin/mpicxx"
-
-PRECISION ?= DP
